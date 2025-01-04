ff4c9597f8bcdc66cd55f007cba4af3089fe5482
ashmita <ashmita0shahi@gmail.com>
2025-01-04 12:30:08 +0545

    F7- app initiated

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors")
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    // maxAge: 3600, // Maximum age of the preflight request cache
+};
+app.use(cors(corsOptions));
+
+// Routes
+app.use("/api/users", require("./routes/UserRoute"));
+// app.use("/api/users", require("./routes/UserProfileRoute"));
+// app.use("/api/profiles", require("./routes/UserProfileRoute"));
+// app.use("/api/posts", require("./routes/PostRoute"));
+// app.use("/api/guidances", require("./routes/GuidanceRoute"));
+// app.use("/api/notifications", require("./routes/NotificationRoute"));
+// app.use("/api/feedbacks", require("./routes/FeedbackRoute"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});

1bde409e6c120e94e6b01ded9c99fcfb079d081a
ashmita <ashmita0shahi@gmail.com>
2025-01-04 16:46:33 +0545

    F13-routing for room

---
+++ routes/RoomRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { protect, staff } = require("../middleware/authMiddleware");
+const {
+    createRoom,
+    getAllRooms,
+    getRoomById,
+    updateRoomAvailability,
+    deleteRoom,
+} = require("../controller/RoomController");
+
+const router = express.Router();
+
+// Public Routes
+router.get("/", getAllRooms); // Get all rooms
+router.get("/:id", getRoomById); // Get room by ID
+
+// Staff Routes
+router.post("/", protect, staff, createRoom); // Create a new room
+router.put("/:id/status", protect, staff, updateRoomAvailability); // Update room availability
+router.delete("/:id", protect, staff, deleteRoom); // Delete a room
+
+module.exports = router;
